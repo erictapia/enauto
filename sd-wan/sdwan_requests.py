a73c89091f5e3acc31ce9753bc24435f1db4579d
Eric Tapia <erictapia1@gmail.com>
2020-12-11 12:12:08 -0800

    Added comments and start of code. Pulled away. To be continue.

---
+++ sd-wan/sdwan_requests.py
@@ -0,0 +1,39 @@
+import requests
+
+
+# Global constants
+
+
+
+# TODO:
+# 	- API requests using Device Intentory API, retrieve and display data
+# 	- API requests using Administration API
+#	- API requests using Configuration API, modify fabric configuration
+#	- API requests using Monitoring API including real-time
+#	- API requests using Troubleshoot API
+
+# Cisco SD-WAN 19.2 Always On 
+#
+# host: https://sandbox-sdwan-1.cisco.com
+# username: devnetuser
+# password: RG!_Yw919_83
+
+
+
+
+# Instantiate a requests session
+host_session = requests.session()
+
+# Authenticate to get a j_session_id
+reponse = host_session.post( )
+
+# Get Device Inventory
+uri = '/device'
+
+
+uri = '/system/device/vedges'
+
+
+
+
+
